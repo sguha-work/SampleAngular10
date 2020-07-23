1b2c75348883d2614ac4d9ff96e1be9c968b0c6e
sguha-work <sguha1988.life@gmail.com>
2020-07-23 14:03:55 +0530

    ui component structer made

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: 'login', component: SignupComponent },
+{ path: 'signup', component: SignupComponent },
+{ path: 'home', component: HomeComponent },
+{ path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TypeComponent } from './components/signup/type/type.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { MenuComponent } from './components/home/menu/menu.component';
+import { ContentComponent } from './components/home/content/content.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    TypeComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    ContentComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
