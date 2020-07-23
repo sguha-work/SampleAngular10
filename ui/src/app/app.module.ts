b4dc607540c645062962ce625b3371109dab50f4
sguha-work <sguha1988.life@gmail.com>
2020-07-23 18:32:09 +0530

    ajax service begins

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MenuComponent } from './components/home/menu/menu.component';
 import { ContentComponent } from './components/home/content/content.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
